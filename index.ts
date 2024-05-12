d32b98361067d9fe794e39682036d58aad98aefe
Yassine B <yassinebenmendour2@gmail.com>
2024-05-12 22:30:45 +0200

    fixing deployment

--- index.ts
+++ index.ts
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.set('views', path.join(__dirname, "views"));
 
-app.set("port", process.env.PORT || 10000);
+app.set("port", 10000);
 
 async function sortActors(sortField: any, sortOrder: any): Promise<Actor[]> {
     return await ActorsCollection.find<Actor>({}).sort({ [sortField]: sortOrder }).toArray();
