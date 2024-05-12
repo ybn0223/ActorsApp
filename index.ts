8d735e78d797e415abfd886f5ee8776525eb0002
Yassine B <yassinebenmendour2@gmail.com>
2024-05-12 22:18:56 +0200

    fixing render.com deployment

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-import express, { Express } from "express";
+import {Express} from "express";
+import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 import {connect, Actors, ActorsCollection} from "./database";
