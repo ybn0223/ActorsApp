75fb9c5d12b1743bd260b954afa4bd3241b25214
Yassine B <yassinebenmendour2@gmail.com>
2024-05-25 01:24:21 +0200

    removed bcrypt from tsconfig for render

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from 'express';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { usersCollection } from '../database';
 import {registerUser} from '../database';
 
