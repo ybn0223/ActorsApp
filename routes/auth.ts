42f8bd94f6d0fffc7b202dd35c806e2fbefd0b65
Yassine B <yassinebenmendour2@gmail.com>
2024-05-25 01:25:26 +0200

    removed minor mistake

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from 'express';
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 import { usersCollection } from '../database';
 import {registerUser} from '../database';
 
